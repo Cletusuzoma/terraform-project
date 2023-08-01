2165a43ff7524f0eff3f5530d7c6a5fa80531eb7
Cletusuzoma <114175112+Cletusuzoma@users.noreply.github.com>
2023-08-01 12:31:30 -0500

    mybranch (#1)

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
+
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {
+    token = "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"
+}
+
+resource "github_repository" "example" {
+  name        = "example"
+  description = "My awesome codebase"
+
+  visibility = "public"
+}
+
 resource "aws_instance" "myec2" {
   ami           =  "ami-0f34c5ae932e6f0e4"
   instance_type = "t3.micro"
