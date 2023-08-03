6ef884bab927dea120ea7b6c1e7c139d3e58a225
Cletus Nwabueze <cletusuzomac@gmail.com>
2023-08-03 16:09:36 -0500

    first ec2

--- main.tf
+++ main.tf
@@ -1,23 +1,7 @@
 
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# Configure the GitHub Provider
-provider "github" {
-    token = "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"
-}
-
-resource "github_repository" "example" {
-  name        = "example"
-  description = "My awesome codebase"
-
-  visibility = "public"
+provider "aws" {
+  region     = "us-east-1"
+ 
 }
 
 resource "aws_instance" "myec2" {
