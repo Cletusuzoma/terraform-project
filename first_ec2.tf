548cbff677185d9784b2810d7967408c2a6f913a
Cletusuzoma <114175112+Cletusuzoma@users.noreply.github.com>
2023-08-03 16:18:41 -0500

    Mybranch1 (#2)
    * mybranch
    
    * first ec2

--- first_ec2.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIA2U2R4ZA77XP7E3XD"
-  secret_key = "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"
-}
-
-
--- main.tf
+++ main.tf
@@ -1,24 +1,7 @@
 
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# Configure the GitHub Provider
-provider "github" {
-    token = "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"
-}
-
-resource "github_repository" "example" {
-  name        = "example"
-  description = "My awesome codebase"
-
-  visibility = "public"
-}
+provider "aws" {
+  region     = "us-east-1"
+ 
 
 resource "aws_instance" "myec2" {
   ami           =  "ami-0f34c5ae932e6f0e4"
